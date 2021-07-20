4fcfe15e8712429ad3c4801dd9b31401fb409b19
tak-dwc <taku3277@gmail.com>
2021-07-20 13:23:06 +0000

    [finish]algorithm

--- air-conditioner.rb
+++ air-conditioner.rb
@@ -2,6 +2,8 @@ input_lines = readlines
 
 room = input_lines[0].to_i
 air =input_lines[1].to_i
+#追加
+power = input_lines[2].to_i
 
 diff = (room - air).abs
 
@@ -15,4 +17,15 @@ elsif diff >= 10 then
   require_time = 60
 end 
 
-p require_time
+ if power == 1
+  require_time 
+ elsif power == 2 then
+  require_time = require_time - 5
+ elsif power == 3 then
+  require_time = require_time - 10
+ else
+ p "風量の値は1~3にしてください"
+ exit
+ end
+  
+  p require_time
